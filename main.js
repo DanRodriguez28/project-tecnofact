c46f89c066c85aade7fa3175daf6da885a98196a
DanRodriguez28 <114537981+DanRodriguez28@users.noreply.github.com>
2025-06-12 07:50:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function homeCarousel() {
+
+    const slides = document.querySelectorAll('.carrusel-slide');
+    let index = 0;
+
+    setInterval(() => {
+      slides[index].classList.remove('active');
+      index = (index + 1) % slides.length;
+      slides[index].classList.add('active');
+    }, 6000);
+
+}
+
+homeCarousel();
