0f5139c27ebe503cef8ac03626c7a6228dba1487
DanRodriguez28 <114537981+DanRodriguez28@users.noreply.github.com>
2025-06-16 09:04:36 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -12,3 +12,17 @@ function homeCarousel() {
 }
 
 homeCarousel();
+
+function password() {
+  
+  const passwordInput = document.getElementById("password");
+  const togglePassword = document.getElementById("togglePassword");
+
+  togglePassword.addEventListener("click", () => {
+    const isPassword = passwordInput.type === "password";
+    passwordInput.type = isPassword ? "text" : "password";
+  });
+
+}
+
+password();
